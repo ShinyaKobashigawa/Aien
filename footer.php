bc91a22d28be5b10fe5a945ea950597edd534028
Kitano Yudai <>
2020-12-05 16:56:16 +0900

    競合直し

--- footer.php
+++ footer.php
@@ -84,10 +84,10 @@
   
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
-  <script src=“https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js”></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- <script src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script> -->
   <script src='<?php echo get_template_directory_uri(); ?>/js/scroll.js'></script>
-  <script src='<?php echo get_template_directory_uri(); ?>/js/under.js'></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/under.js"></script>
 
  </body>
   </html>
--- header.php
+++ header.php
@@ -16,9 +16,9 @@
 <!-- top -->
 
 <body>
-<script src=“https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js”></script>
-<script src=“https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js”></script>
-<script src=“https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js”></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src='<?php echo get_template_directory_uri(); ?>/js/scroll.js'></script>
   
 <div class="wrap-video">
