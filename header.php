280d9b10b591bb479792e1109e1bc3bf992f487b
Kitano Yudai <>
2020-12-06 17:01:03 +0900

    中途半端

--- header.php
+++ header.php
@@ -43,45 +43,122 @@
   </ul>
 </nav>
 
+<!-- <h1 class="wp">最新の投稿</h1> -->
+
+<?php if (have_posts()): ?>
+  <!-- <h2 class="subtitle">最新の投稿</h2> -->
+
 <div class="manth">
   <div class="manth2">
   <h1 class="title">今月の予定</h1>
     <div class="plan">
       <div class="sentence">
-        <p>【GoToトラベル適用！】</p><br><br>
+      <ul>
+      <!-- while (繰り返しの条件。条件が満たされたら繰り返す) -->
+      <!-- have_post()投稿された記事が存在する間 -->
+      <!-- the_post();記事を一つ取り出してつい変えるようにする -->
+        <?php while (have_posts()): the_post(); ?>
+          <li id="post-<?php the_ID(); ?>" class="news">
+            <p>
+              <a href="#"><?php the_title(); //タイトル ?></a>
+            </p>
+
+            <p>
+              <!-- こんにちは！寒い日が続いていますが... -->
+              <?php the_excerpt(); //本文伐採 ?>
+              <a href="<?php the_permalink(); //詳細へのリンク ?>">続きを読む</a>
+            </p>
+          </li>
+
+        <?php endwhile; ?>
+      </ul>
+<?php else: ?>
+  <h2 class="subtitle">投稿はありません</h2>
+<?php endif; ?>
+
+        <!-- <p>【GoToトラベル適用！】</p><br><br>
         <p>巷で話題のGoToトラベル</p><br>
         <p>長い審査を経て、この度</p><br>
         <p>直接予約も適用となりました！！</p><br>
         <p>AIEN 公式HPからご予約すると</p><br>
-        <p>≪35％オフ≫となります！！</p><br>
+        <p>≪35％オフ≫となります！！</p><br> -->
+
+
+
       </div>
     </div>
   </div>
 
+  
 
 
+<?php if (have_posts()): ?>
   <div class="manth2">
   <h1 class="title">イベント</h1>
     <div class="plan">
       <div class="sentence">
-        <p>【Shopifyパートナー交流会】</p><br>
+      <ul>
+      <!-- while (繰り返しの条件。条件が満たされたら繰り返す) -->
+      <!-- have_post()投稿された記事が存在する間 -->
+      <!-- the_post();記事を一つ取り出してつい変えるようにする -->
+        <?php while (have_posts()): the_post(); ?>
+          <li id="post-<?php the_ID(); ?>" class="news">
+            <p>
+              <a href="#"><?php the_title(); //タイトル ?></a>
+            </p>
+
+            <p>
+              <!-- こんにちは！寒い日が続いていますが... -->
+              <?php the_excerpt(); //本文伐採 ?>
+              <a href="<?php the_permalink(); //詳細へのリンク ?>">続きを読む</a>
+            </p>
+          </li>
+
+        <?php endwhile; ?>
+      </ul>
+<?php else: ?>
+  <h2 class="subtitle">投稿はありません</h2>
+<?php endif; ?>
+        <!-- <p>【Shopifyパートナー交流会】</p><br>
         <p>Shopifyを仕事にしている人</p><br>
         <p>またはShopifyをこれから仕事にしたい方の</p><br>
-        <p>交流イベントを開催します</p><br>
+        <p>交流イベントを開催します</p><br> -->
       </div>
     </div>
   </div>
 
 
-
+<?php if (have_posts()): ?>
   <div class="manth2">
   <h1 class="title">お知らせ</h1>
     <div class="plan">
       <div class="sentence">
-        <p>【3rd Anniversary】</p><br>
+      <ul>
+      <!-- while (繰り返しの条件。条件が満たされたら繰り返す) -->
+      <!-- have_post()投稿された記事が存在する間 -->
+      <!-- the_post();記事を一つ取り出してつい変えるようにする -->
+        <?php while (have_posts()): the_post(); ?>
+          <li id="post-<?php the_ID(); ?>" class="news">
+            <p>
+              <a href="#"><?php the_title(); //タイトル ?></a>
+            </p>
+
+            <p>
+              <!-- こんにちは！寒い日が続いていますが... -->
+              <?php the_excerpt(); //本文伐採 ?>
+              <a href="<?php the_permalink(); //詳細へのリンク ?>">続きを読む</a>
+            </p>
+          </li>
+
+        <?php endwhile; ?>
+      </ul>
+<?php else: ?>
+  <h2 class="subtitle">投稿はありません</h2>
+<?php endif; ?>
+        <!-- <p>【3rd Anniversary】</p><br>
         <p>合縁奇縁の4年目になりました</p><br>
         <p>4年目のAIENも</p><br>
-        <p>何卒宜しくお願い致します</p><br>
+        <p>何卒宜しくお願い致します</p><br> -->
       </div>
     </div>
   </div>
